459679de471701913b9703f53756e19366d8e023
Andres Rendon <89153034+RedWizard-Codes@users.noreply.github.com>
2021-08-26 19:20:44 -0400

    Update bank_user.py
    Made a second class for the bank. and some other fuctions. 
    
    still cant get some stuff to do what i need it to.

--- bank_user.py
+++ bank_user.py
@@ -1,35 +1,42 @@
-# make_withdrawal(self, amount) - have this method decrease the user's balance by the amount 
-# specified
-
-# display_user_balance(self) - have this method print the user's name and account balance 
-# to the terminal
-# eg. "User: Guido van Rossum, Balance: $150
-
-# BONUS: transfer_money(self, other_user, amount) - have this method decrease the user's 
-# balance by the amount and add that amount to other other_user's balance
-
-# Practice creating a class and making instances from it
-# Practice accessing the methods and attributes of different instances
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 class User:
     def __init__(self,name,email):
         self.name = name
         self.email = email
-        self.balance = 0
+
+guido = User("guido", "email")
+monty = User('monty', 'email')
+Andres = User('Andres', 'email')
+
+import random
+a=random.randrange(100000)
+class BankAccount:
+    def __init__(self, int_rate, balance): 
+        self.bank_id = a
         self.bank_name = "First National Dojo"
+        self.balance = 0
+        self.int_rate = .001
     def deposit(self, amount):
         self.balance += amount
-    def make_withdrawal(self, amount):
+    def withdrawal(self, amount):
         self.balance -= amount
     def transfer(self,user,amount):
         user.balance += amount
         self.balance -= amount
+    # def display_account_info(self):
+        # your code here
+        # print(self.BankAccount)
+    # def yield_interest(self):
+
+Andres=BankAccount(.05,400)
+
 
 guido = User("guido", "email")
 guido.bank_name = "Dojo Credit Union"
 guido.name = "Guido"
 guido.deposit(1000)
+guido.deposit(1000)
+guido.deposit(1000)
+guido.withdrawal(550)
 print(guido.balance)
 print(guido.bank_name)
 print(guido.name)
@@ -37,11 +44,19 @@ print(guido.name)
 monty = User('monty', 'email')
 monty.name = "Monty"
 monty.deposit(1000)
+monty.deposit(46000)
+monty.withdrawal(300)
+monty.withdrawal(300)
+monty.withdrawal(300)
+monty.withdrawal(300)
 monty.transfer(guido,350)
 print(monty.balance)
 print(monty.name)
 print(monty.bank_name)
 print(guido.balance)
+
+
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # how do i make the guidobalance stay updated?
 # i tried to make a second class "bank" and i couldnt figure out how to reference a seperate class.
+# i need a yield interest def? i dont even know where to start
