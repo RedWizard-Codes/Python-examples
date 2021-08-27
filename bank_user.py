30a406a11c7e026c0c15cd15e5cc2cd34331287c
Andres Rendon <89153034+RedWizard-Codes@users.noreply.github.com>
2021-08-27 01:50:14 -0400

    Update bank_user.py
    added the separate classes.
    made a couple changes. how do i save multiple iterations of main? or access old iterations before commits were made on the main in git?

--- bank_user.py
+++ bank_user.py
@@ -1,62 +1,75 @@
+import random
+a=random.randrange(100000)
+class BankAccount:
+    accounts = []
+    def __init__(self, int_rate, balance): 
+        self.bank_id = a
+        self.bank_name = "First National Dojo"
+        self.balance = balance
+        self.int_rate = int_rate
+        BankAccount.accounts.append(self)
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdrawal(self, amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    def transfer(self,user,amount):
+        user.balance += amount
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
 class User:
     def __init__(self,name,email):
         self.name = name
         self.email = email
+        self.account = BankAccount(.01, 500)
 
-guido = User("guido", "email")
-monty = User('monty', 'email')
+
 Andres = User('Andres', 'email')
-
-import random
-a=random.randrange(100000)
-class BankAccount:
-    def __init__(self, int_rate, balance): 
-        self.bank_id = a
-        self.bank_name = "First National Dojo"
-        self.balance = 0
-        self.int_rate = .001
-    def deposit(self, amount):
-        self.balance += amount
-    def withdrawal(self, amount):
-        self.balance -= amount
-    def transfer(self,user,amount):
-        user.balance += amount
-        self.balance -= amount
-    # def display_account_info(self):
-        # your code here
-        # print(self.BankAccount)
-    # def yield_interest(self):
-
-Andres=BankAccount(.05,400)
-
+print(Andres.account.balance)
+print(Andres.account.bank_name)
+print(Andres.name)
+Andres.savings = BankAccount(.01,3000)
+Andres.savings.name= 'savings'
 
 guido = User("guido", "email")
 guido.bank_name = "Dojo Credit Union"
 guido.name = "Guido"
-guido.deposit(1000)
-guido.deposit(1000)
-guido.deposit(1000)
-guido.withdrawal(550)
-print(guido.balance)
-print(guido.bank_name)
+print(guido.account.balance)
+print(guido.account.bank_name)
 print(guido.name)
 
 monty = User('monty', 'email')
 monty.name = "Monty"
-monty.deposit(1000)
-monty.deposit(46000)
-monty.withdrawal(300)
-monty.withdrawal(300)
-monty.withdrawal(300)
-monty.withdrawal(300)
-monty.transfer(guido,350)
-print(monty.balance)
+# monty.transfer(guido,350)
+print(monty.account.balance)
 print(monty.name)
-print(monty.bank_name)
-print(guido.balance)
+print(monty.account.bank_name)
 
+
+
+guido.account.deposit(10).deposit(20).deposit(40).withdrawal(600).yield_interest()
+monty.account.deposit(100).deposit(200).deposit(400).withdrawal(60).yield_interest()
+
+BankAccount.print_all_accounts()
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # how do i make the guidobalance stay updated?
-# i tried to make a second class "bank" and i couldnt figure out how to reference a seperate class.
-# i need a yield interest def? i dont even know where to start
+# transfer not working anymore and no idea why. it did before and it was in the bank class so no movement. mmmm
+# tried to make a bank id. shit looks hard. ie. need to save to each a "random" number and not repeat, encrypt maybe, decryptor... we shall see later
